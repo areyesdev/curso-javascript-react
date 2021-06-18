760054dfde239f3959d1feb759efc9f2aeac9a80
Andres Reyes 👋 <ar97yf@gmail.com>
2021-06-17 21:11:49 -0500

    :sparkles: Array prototype reduce
    Array prototype reduce Normalizando datos

--- arrays-block-buster/js/search.js
+++ arrays-block-buster/js/search.js
@@ -3,16 +3,34 @@ import render from "./render.js";
 
 const search = window["search-form"];
 
-search.addEventListener("submit", function (ev) {
-  ev.preventDefault();
+search.addEventListener("submit", function (event) {
+  event.preventDefault();
   const formData = new FormData(this);
-  const title = formData.get("title");
-  const movies = filterByTitle(title);
-  render(movies);
+  const query = formData.get("title");
+  const movies = query.trim() !== "" ? searchMovie(query) : [];
+  if (movies.length > 0) {
+    return render(movies);
+  }
+  return alert("No encontramos la pelicula");
 });
 
 function filterByTitle(title) {
   return movies.filter((movie) => {
     return movie.title.toLowerCase().includes(title.toLowerCase());
   });
+}
+
+function findById(id) {
+  // return movies.filter((movie) => {
+  return movies.find((movie) => {
+    return movie.id === parseInt(id, 10);
+  });
+}
+
+function searchMovie(query) {
+  if (isNaN(query)) {
+    return filterByTitle(query);
+  }
+  return [findById(query)];
+  // return findById(query)
 }
