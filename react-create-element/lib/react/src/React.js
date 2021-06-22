afd53a1abe4c96e55e6176b9041e07b474d64955
Andres Reyes 👋 <ar97yf@gmail.com>
2021-06-22 16:08:11 -0500

    ✨[ADD] Añadiendo el ciclo de vida

--- react-create-element/lib/react/src/React.js
+++ react-create-element/lib/react/src/React.js
@@ -6,6 +6,17 @@ class Component {
   update() {}
   #updater(){
     this.update(this.render())
+    this.componentDidUpdate()
+  }
+  // se llama andes que se renderice el componente
+  componentWillMount() {
+
+  }
+  componentDidMount() {
+
+  }
+  componentDidUpdate() {
+
   }
   setState(newState) {
     this.state = {
@@ -13,6 +24,10 @@ class Component {
       ...newState
     }
     this.#updater()
+  }
+  build() {
+    this.componentWillMount()
+    return this.render()
   }
 }
 
