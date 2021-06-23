0655d0c6acbfab4ec0cd16bf93445d65832850e3
Andres Reyes 👋 <ar97yf@gmail.com>
2021-06-23 18:49:49 -0500

    :sparkles: [ADD] redux

--- block-buster/src/components/movie-list.js
+++ block-buster/src/components/movie-list.js
@@ -1,6 +1,7 @@
 import { Component } from "../lib/react/index.js";
 import styled from "../lib/styled-components.js";
-import movies from "../movies.js";
+//import movies from "../movies.js";
+import store from "../store.js";
 import Movie from "./movie.js";
 import Wrapper from "./wrapper.js";
 
@@ -13,7 +14,11 @@ const MovieListStyled = styled.section`
 `;
 
 class MovieList extends Component {
+  state = {
+    movies: store.getState().movieList,
+  };
   render() {
+    const { movies } = this.state;
     return Wrapper({
       children: MovieListStyled({
         children: movies.map((movie) => new Movie(movie)),
