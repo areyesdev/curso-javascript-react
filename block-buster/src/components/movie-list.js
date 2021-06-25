433d646f89128e6878ebfe636dcbb7f6a7c00424
Andres Reyes 🐱‍💻 <ar97yf@gmail.com>
2021-06-25 11:51:10 -0500

    :sparkles: Infinite Scroll con Intersection Observer

--- block-buster/src/components/movie-list.js
+++ block-buster/src/components/movie-list.js
@@ -19,24 +19,36 @@ class MovieList extends Component {
     page: 1,
   };
   getPage = async (page) => {
-    const { results } = await api.moviePage(1);
+    const { results } = await api.moviePage(page);
     store.dispatch({
       type: ADD_MOVIES,
       payload: results,
     });
   };
+  handleIntersection = (entries) => {
+    if (entries[0].isIntersecting) {
+      this.getPage(this.state.page);
+      this.setState({
+        page: this.state.page + 1,
+      });
+    }
+  };
   componentDidMount() {
-    this.getPage(this.state.page);
+    //this.getPage(this.state.page);
     store.subscribe(() => {
-      console.log("me he actualizado");
+      //console.log("me he actualizado");
       this.setState();
     });
+    const observer = new IntersectionObserver(this.handleIntersection, {
+      rootMargin: "50px",
+    });
+    observer.observe(window.intersector);
   }
   render() {
     const state = store.getState();
     const movieListId = state.list[state.filter];
     const movieList = state.movieList;
-    console.log(state);
+    //console.log(state);
     return Wrapper({
       children: MovieListStyled({
         children: movieListId.map((id) => new Movie(movieList.get(id))),
