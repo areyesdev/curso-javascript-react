49394a93016824f018d4a1f48905616db89cf553
Andres Reyes 🐱‍💻 <ar97yf@gmail.com>
2021-06-25 11:34:08 -0500

    :sparkles: [ADD] Información de la API

--- block-buster/src/components/movie-list.js
+++ block-buster/src/components/movie-list.js
@@ -1,10 +1,10 @@
 import { Component } from "../lib/react/index.js";
 import styled from "../lib/styled-components.js";
-//import movies from "../movies.js";
 import store from "../store.js";
 import Movie from "./movie.js";
 import Wrapper from "./wrapper.js";
 import api from "../api.js";
+import { ADD_MOVIES } from "../actions/index.js";
 
 const MovieListStyled = styled.section`
   display: grid;
@@ -15,10 +15,22 @@ const MovieListStyled = styled.section`
 `;
 
 class MovieList extends Component {
-  state = {};
-  async componentDidMount() {
-    const page10 = await api.moviePage(10);
-    console.log(page10);
+  state = {
+    page: 1,
+  };
+  getPage = async (page) => {
+    const { results } = await api.moviePage(1);
+    store.dispatch({
+      type: ADD_MOVIES,
+      payload: results,
+    });
+  };
+  componentDidMount() {
+    this.getPage(this.state.page);
+    store.subscribe(() => {
+      console.log("me he actualizado");
+      this.setState();
+    });
   }
   render() {
     const state = store.getState();
